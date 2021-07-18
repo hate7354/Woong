4251b52d2e1be4a7b391cbfca30514077fcbc677
Jin\User <hate7354@naver.com>
2021-07-18 22:55:20 +0900

    change 바뀝니다

--- Woong/src/Woong/Woong.java
+++ Woong/src/Woong/Woong.java
@@ -5,6 +5,8 @@ public class Woong {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("마스터아닙니다");
+		System.out.println("바뀝니다");
+		System.out.println("아무거나");
 	}
 
 }
