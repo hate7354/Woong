1fc2b306e2bf35f62a43db8b08e0ff5fd71294ac
hoogun <hoogun@DESKTOP-RURVHJP>
2021-07-18 23:09:49 +0900

    message fdsfsdfsd

--- Woong/src/Woong/Woong.java
+++ Woong/src/Woong/Woong.java
@@ -4,9 +4,10 @@ public class Woong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("마스터아닙니다");
-		System.out.println("바뀝니다");
-		System.out.println("아무거나");
+		System.out.println("留덉뒪�꽣�븘�떃�땲�떎");
+		System.out.println("諛붾�앸땲�떎");
+		System.out.println("�븘臾닿굅�굹");
+		System.out.println("fsdfdsfdsf");
 	}
 
 }
